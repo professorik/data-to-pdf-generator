0f742ef3c370a27d7e88816af824066fac211b83
professorik <36134599+professorik@users.noreply.github.com>
2021-09-28 13:00:05 +0300

    patch

--- src/api/services/PageService.ts
+++ src/api/services/PageService.ts
@@ -33,7 +33,7 @@ async function getRecipesFromDB() {
 
     await client.release();
     //run pdf generator script
-    await child_process.execSync('next build && next export && node src/convert.tsx');
+    await child_process.execSync('cross-env NODE_OPTIONS=--max_old_space_size=4096 next build && next export && node --max_old_space_size=4096 src/convert.tsx');
 }
 
 module.exports = {getRecipesFromDB}
